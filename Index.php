8b6ecc8d999b009486001fe8e705a903a2fddfa6
Andolo-64 <andreasolavhogestol@gmail.com>
2024-02-22 07:55:42 +0100

    first commit

---
+++ Index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title>Nettverk</title>
+</head>
+<body>
+<div>
+  <h1>  Plan A suksess <h1> 
+</div>
+<div>
+<img src="Netwerk.png" width="1450" height="900">
+
+</div>
+<div>
+ <h2>   Plan B failed <h2>
+</div>
+<div>
+<img id="han" src="Finn.png" width="1200" height="800">
+
+
+<div>
+
+
+
+</body>
+</html>
